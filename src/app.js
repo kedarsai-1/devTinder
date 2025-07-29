2b08a2f901ac4ce33dd9f7c75aa28426794b6a06
kedarsai-1 <saikedar2003@gmail.com>
2025-07-29 22:26:08 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
-app.use("hello",(req,res)=>{
-res.send('hello world');
-}
-);
-app.use("/home",(req,res)=>{
-    res.send('hell0 ');
-});
-app.use("/zero",(req,res)=>{
-    res.send('hell0 hello');
-});
 
+app.get("/user",(req,res)=>{
+    res.send({firstname:"kedar",lastname:"Nannapaneni"})
+})
+app.post("/user",(req,res)=>{
+    res.send("Data successfully savec in database")
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+})
 app.listen(4000,()=>{
     console.log('server is sucessfully listening on port 4000')
 });
